0789b2caa4252440758a192e5c8f027bf40ae623
coenaerts.kenzo@gmail.om <Ikbendebest5>
2021-01-19 22:23:52 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+//todo: uninstall aos package
+import React from 'react';
+import {useRef, useLayoutEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Header from './Header'
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(window.scrollY);
+
+        return (
+            <div className="App">
+                <Header/>
+                <About/>
+                <Projects/>
+                <Footer/>
+            </div>
+        )
+    }
 }
 
 export default App;
+//  <Contact/>
